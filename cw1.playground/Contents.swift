2a58c5154436da3968f73a623c6d4074e6468dbc
sherifaaljassem <70379109+sherifaaljassem@users.noreply.github.com>
2020-09-09 20:41:37 +0300

    changed

---
+++ cw1.playground/Contents.swift
@@ -0,0 +1,16 @@
+let name = "sherifa"
+var hobby1 = "drawing"
+var hobby2 = "reading"
+var age = 17
+var height =  168.0
+print ("my name is \( name ) and my hobby is \( hobby1) and \( hobby2) my age is \( age ) and my height is \( height )")
+
+let friendName = "sara"
+var saraHobby1 = "reading"
+var saraHobby2 = "coding"
+var age2 = 16
+var height2 = 158.0
+var ageDifference = (age)-(age2)
+print (ageDifference)
+var mutualHobby = (hobby2) == (saraHobby1) || (hobby2) == (saraHobby2) || (hobby1) == (saraHobby1) || (hobby1) == (saraHobby2)
+
